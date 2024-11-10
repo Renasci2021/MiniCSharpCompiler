aa59acbd4cddeb22fe9340153e46a01ad1883bbd
Rena <lihaoxua21@mails.tsinghua.edu.cn>
2024-11-11 01:28:49 +0800

    test: enhance LexerTests and update README.md
    - Added test files and unit test information to README.md
    - Enhanced LexerTests.cs with additional debug information and separators for better observation of each test case output

--- src/MiniCSharpCompiler.Test/LexerTests.cs
+++ src/MiniCSharpCompiler.Test/LexerTests.cs
@@ -7,13 +7,28 @@ public class LexerTests
     [Test]
     public async Task LexerTest()
     {
-        // * Get all the files in the TestFiles directory
         string[] files = Directory.GetFiles("../../../../MiniCSharpCompiler.Test/TestFiles", "*.cs");
 
+        bool allFilePassed = true;
         foreach (var file in files)
         {
-            await TestFile(file);
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine($"Testing file: {file}");
+            Console.WriteLine();
+
+            try
+            {
+                await TestFile(file);
+                Console.WriteLine($"File passed: {file}");
+            }
+            catch (Exception e)
+            {
+                allFilePassed = false;
+                Console.WriteLine($"Error testing file: {file}");
+                Console.WriteLine(e.Message);
+            }
         }
+        await Assert.That(allFilePassed).IsTrue();
     }
 
     private async Task TestFile(string filePath)
@@ -25,10 +40,15 @@ public class LexerTests
         var tokens = lexer.Tokenize(sourceCode).ToList();
         var standardTokens = standardLexer.Tokenize(sourceCode).ToList();
 
+        Console.WriteLine($"Token count: {tokens.Count}");
+
         await Assert.That(tokens.Count).IsEqualTo(standardTokens.Count);
 
         for (int i = 0; i < tokens.Count; i++)
         {
+            Console.WriteLine($"Token {i}: {tokens[i].Kind} - {tokens[i].Value}");
+            Console.WriteLine($"Standard Token {i}: {standardTokens[i].Kind} - {standardTokens[i].Value}");
+
             await Assert.That(tokens[i].Kind).IsEqualTo(standardTokens[i].Kind);
             await Assert.That(tokens[i].Value).IsEqualTo(standardTokens[i].Value);
         }
